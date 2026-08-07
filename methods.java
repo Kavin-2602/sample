1cf2ac6d70c848f9073d3241237b11e14e5c561a
Kavin-2602 <kavin26official@gmail.com>
2026-08-07 13:38:29 +0530

    hello and methods are created

---
+++ methods.java
@@ -0,0 +1,21 @@
+
+
+public class methods {
+	// Addition Method
+	void addition() {
+		int a = 10;
+		int b = 20;
+		int c = a+b ;
+	System.out.println(" Result C =" +c);
+	}
+	
+	/* Main class */
+	public static void main(String[] args) {
+		
+		Methods C = new Methods();
+		C.addition();
+		
+		}
+	
+
+}
